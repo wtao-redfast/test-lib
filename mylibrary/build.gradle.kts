185578fc06c56462e23969e9aa6f3ff5b0da4895
wtao <wtao@redfast.com>
2025-02-25 10:14:59 -0800

    delete app

--- mylibrary/build.gradle.kts
+++ mylibrary/build.gradle.kts
@@ -37,7 +37,7 @@ publishing {
         create<MavenPublication>("maven") {
             groupId = "com.test"
             artifactId = "mylibrary"
-            version = "0.0.1"
+            version = "0.0.2"
 
             afterEvaluate{
                 from(components["release"])
